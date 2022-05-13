8dd63d01a17d416dd1bf28fd71a645ffd1414dcc
Monica du Toit <59988997+MonicaDuToit@users.noreply.github.com>
2022-05-13 14:57:22 +1000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,18 @@ var sectionHeight = function() {
   }
 }
 
+// tree view toggler
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+//
+
 $(window).resize(sectionHeight);
 
 $(function() {
