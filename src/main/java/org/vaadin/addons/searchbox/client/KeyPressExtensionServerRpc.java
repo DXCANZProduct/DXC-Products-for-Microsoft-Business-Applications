0a6b046adf310ac33a8243c1f92975d65028a0a5
Monica du Toit <jdutoit2@dxc.com>
2022-05-25 16:34:29 +1000

    -

--- src/main/java/org/vaadin/addons/searchbox/client/KeyPressExtensionServerRpc.java
+++
@@ -1,8 +0,0 @@
-package org.vaadin.addons.searchbox.client;
-
-import com.vaadin.shared.communication.ServerRpc;
-
-public interface KeyPressExtensionServerRpc extends ServerRpc {
-
-    void enterPressed(String searchTerm);
-}
